a6634b6f21a3a21947c407150467a326ba4cb31d
adhanji <aleem@chalk.com>
2015-04-23 23:46:51 -0400

    WIP: Chapter 1 Title screen

---
+++ AleemDhanji/ChapterTitleViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ChapterTitleViewController.swift
+//  AleemDhanji
+//
+//  Created by Aleem Dhanji on 2015-04-23.
+//  Copyright (c) 2015 Aleem Dhanji. All rights reserved.
+//
+
+import UIKit
+
+class ChapterTitleViewController: UIViewController {
+
+    // MARK - Variables
+
+    @IBOutlet weak var openQuote: UILabel?
+    @IBOutlet weak var closeQuote: UILabel?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
