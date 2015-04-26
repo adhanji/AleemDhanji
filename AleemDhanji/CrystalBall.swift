7bb74c99c4d0c9c6faa0994787725d9afc387b5a
adhanji <aleem@chalk.com>
2015-04-26 13:04:18 -0400

    refactor: update copy

--- AleemDhanji/CrystalBall.swift
+++ AleemDhanji/CrystalBall.swift
@@ -10,7 +10,18 @@ import Foundation
 
 class CrystalBall {
 
-    private let predictions = ["I doubt it!", "Why not eh?", "Don't get your hopes up", "All signs say yes!"]
+    private let predictions = [
+        "It is certain",
+        "It is decidedly so",
+        "All signs say YES",
+        "The stars are not aligned",
+        "My reply is no",
+        "It is doubtful",
+        "Better not tell you now",
+        "Concentrate and ask again",
+        "Unable to answer now",
+        "It is hard to say"
+    ]
 
     func randomPrediction() -> String {
         let randomIndex = arc4random_uniform(UInt32(predictions.count))
