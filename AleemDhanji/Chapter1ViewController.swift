36a14afba26ed8ae8eda3ecc3bbeb4b5a4d7e649
adhanji <aleem@chalk.com>
2015-04-26 09:09:52 -0400

    feat: chapter 1 slider interaction

--- AleemDhanji/Chapter1ViewController.swift
+++ AleemDhanji/Chapter1ViewController.swift
@@ -15,28 +15,50 @@ class Chapter1ViewController: UIViewController {
     @IBOutlet weak var buildings: UIImageView!
     @IBOutlet weak var buildingsOutline: UIImageView!
     @IBOutlet weak var laptop: UIImageView!
-    @IBOutlet weak var slider: UIImageView!
-    @IBOutlet weak var slideButton: UIImageView!
+    @IBOutlet weak var sliderTrack: UIImageView!
     @IBOutlet weak var destinationPlaceholder: UIImageView!
+    @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var shines: UIImageView!
     @IBOutlet weak var brackets: UILabel!
 
+    // MARK: - Variables
+
+    var slideButton: UIImage!
     var initialHiddenGroup: [UIView] = []
     var transitionGroup1: [UIView] = []
     var transitionGroup2: [UIView] = []
+    var showOnSlide: [UIView] = []
+    var hideOnSlide: [UIView] = []
 
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        initialHiddenGroup = [buildings, buildingsOutline, laptop, slider, slideButton, destinationPlaceholder, shines, brackets]
+
+
+        // Hide these first
+        initialHiddenGroup = [buildings, buildingsOutline, laptop, sliderTrack, slider, destinationPlaceholder, shines, brackets]
+        // First fade in these
         transitionGroup1 = [buildings, buildingsOutline]
-        transitionGroup2 = [laptop, slider, slideButton, destinationPlaceholder]
+        // Then fade in these
+        transitionGroup2 = [laptop, sliderTrack, slider, destinationPlaceholder]
+        // Show these when slider reaches destination
+        showOnSlide = [shines, brackets]
+        // Hide these when slider reaches destination
+        hideOnSlide = [buildingsOutline, sliderTrack, slider, destinationPlaceholder]
 
         // Allow tint color to be changed in Interface Builder
         buildings.image = buildings.image!.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
         laptop.image = laptop.image!.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+
+        let trackColor = UIColor(red: 41/255, green: 41/255, blue: 41/255, alpha: 1)
+        slider.minimumTrackTintColor = trackColor
+        slider.maximumTrackTintColor = trackColor
+
+        slideButton = UIImage(named: "slide-button")
+        slider.setThumbImage(slideButton, forState: UIControlState.Normal)
+        slider.setThumbImage(slideButton, forState: UIControlState.Highlighted)
 
         for item in initialHiddenGroup {
             item.alpha = 0
@@ -44,12 +66,14 @@ class Chapter1ViewController: UIViewController {
     }
 
     override func viewDidAppear(animated: Bool) {
-        UIView.animateWithDuration(1, delay: 2, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+        // Fade in Group1
+        UIView.animateWithDuration(0.75, delay: 1, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
             for item in self.transitionGroup1 {
                 item.alpha = 1
             }
         }, completion: nil)
-        UIView.animateWithDuration(1, delay: 3, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+        // Then fade in Group2
+        UIView.animateWithDuration(0.75, delay: 2, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
             for item in self.transitionGroup2 {
                 item.alpha = 1
             }
@@ -60,16 +84,25 @@ class Chapter1ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
+    // MARK: UISlider Actions
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func didReachDestination() {
+        if slider.value == 1 {
+            // Switch to green state
+            UIView.animateWithDuration(0, delay: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
+                for item in self.showOnSlide {
+                    item.alpha = 1
+                }
+                self.laptop.tintColor = UIColor(red: 69/255, green: 219/255, blue: 90/255, alpha: 1)
+            }, completion: nil)
+            UIView.animateWithDuration(0, delay: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
+                for item in self.hideOnSlide {
+                    item.alpha = 0
+                }
+            }, completion: nil)
+        }
     }
-    */
+
 
 }
